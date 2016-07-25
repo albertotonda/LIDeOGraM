f80905ee1ab7277d10f38b25ed4995affb772204
Arkantus <barnabe.marc@gmail.com>
2016-07-25 15:56:39 +0200

    Started observer

---
+++ observers.py
@@ -0,0 +1,13 @@
+class Observer:
+    def update(self):
+        pass
+
+class Subject:
+    def registerObs(self,o):
+        pass
+
+    def removeObs(self,o):
+        pass
+
+    def notifyObs(self):
+        pass
