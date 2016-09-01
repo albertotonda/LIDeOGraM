daeb7a408f2321a5e09220705ae1feeb4c531851
tchabin <Pwet&124>
2016-09-01 15:44:10 +0200

    Version instable contenant l'optimisation du modèle global, son affichage, et la possibilité donné à l'utilisateur de le modifier

--- optimModGlobal.py
+++ optimModGlobal.py
@@ -5,8 +5,9 @@ import fitness
 from deap import base
 from deap import creator
 from deap import tools
-
-
+from deap import algorithms
+import numpy as np
+import pickle
 
 class OptimModGlobal:
 
@@ -17,7 +18,7 @@ class OptimModGlobal:
     def initIndividual(self,indv, sizes):
         vect = []
         for size_i in sizes:
-            if (size_i - 1 > 0):
+            if (size_i > 0):
                 vect.append(random.randint(0, size_i - 1))
             else:
                 vect.append(-1)
@@ -34,16 +35,36 @@ class OptimModGlobal:
     def mutEqua(self,indv, probmut):
         for i in range(len(indv)):
             if (random.random() < probmut):
-                if (self.sizes[i] - 1 < 1):
+                if (self.sizes[i]  < 1):
                     indv[i] = -1
                 else:
                     indv[i] = random.randint(0, self.sizes[i] - 1)
 
+    def mutEqua2(self, indv, probmut,sizes):
+        for i in range(len(indv)):
+            if (random.random() < probmut):
+                if (sizes[i] < 1):
+                    indv[i] = -1
+                else:
+                    if sizes[i] > 1 :
+                        if (indv[i]==0):
+                            indv[i] = 1
+                        elif (indv[i]==sizes[i]-1):
+                            indv[i] = int(sizes[i]-2)
+                        else:
+                            indv[i] = indv[i] + random.randint(0,1)*2-1
+        return (indv,)
+
+
     def startOptim(self):
+
+
+
+
         random.seed()
 
-        creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-        creator.create("Individual", list, fitness=creator.FitnessMax)
+        creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+        creator.create("Individual", list, fitness=creator.FitnessMin)
 
         self.sizes=[]
         for v,s in self.modApp.varEquasize:
@@ -52,51 +73,126 @@ class OptimModGlobal:
         toolbox = base.Toolbox()
         toolbox.register("new_individual",self.initIndividual,creator.Individual,self.sizes)
         toolbox.register("mate",tools.cxUniform,indpb=0.5)
-        toolbox.register("mutate",self.mutEqua,probmut=0.25)
-        toolbox.register("select",tools.selTournament,tournsize=3)
+        toolbox.register("mutate",self.mutEqua2,probmut=0.05,sizes=self.sizes)
+        toolbox.register("select",tools.selTournament,tournsize=2)
         toolbox.register("new_population",tools.initRepeat,list,toolbox.new_individual)
         toolbox.register("evaluate",self.evaluate)
 
+        stats = tools.Statistics(key=lambda ind: ind.fitness.values)
+        stats.register("avg", np.mean)
+        stats.register("std", np.std)
+        stats.register("min", np.min)
+        stats.register("max", np.max)
+        logbook = tools.Logbook()
 
-        NGEN=100
+        with open('bestIndv.dat', 'rb') as f:
+            ret = pickle.load(f)
+        return ret
+
+
+
+        NGEN=200
         CXPB=0.8
-        MUTPB=0.1
-        pop=toolbox.new_population(n=100)
+        MUTPB=0.2
+        mu=200
+        lmbd=150
+        halloffame=tools.HallOfFame(1)
+        pop=toolbox.new_population(n=mu)
         fitnesses=toolbox.map(toolbox.evaluate, pop)
         for ind, fit in list(zip(pop, fitnesses)):
+            print(fit)
             ind.fitness.values = fit
-        for g in range(NGEN):
-            # Select the next generation individuals
-            offspring = toolbox.select(pop, len(pop))
-            # Clone the selected individuals
-            #print(offspring)
-            offspring = list(map(toolbox.clone, offspring))
+        pop,logbook=algorithms.eaMuPlusLambda(pop,toolbox,mu,lmbd,CXPB,MUTPB,NGEN,stats=stats,halloffame=halloffame,verbose=True)
+        pass
+        # record = stats.compile(pop)
+        # popfit = [ind.fitness.values for ind in pop]
+        # logbook.record(gen=0, evals=mu, popfitnesses=popfit,pop=pop,**record)
+        # for g in range(NGEN):
+        #     # Select the next generation individuals
+        #     offspring = toolbox.select(pop, lmbd)
+        #     # Clone the selected individuals
+        #     #print(offspring)
+        #     offspring = list(map(toolbox.clone, offspring))
+        #
+        #     # Apply crossover on the offspring
+        #     #print(list(offspring))
+        #     #print(offspring[::2])
+        #     #print(offspring[1::2])
+        #     #print(zip(offspring[::2], offspring[1::2]))
+        #     for child1, child2 in list(zip(offspring[::2], offspring[1::2])):
+        #         if random.random() < CXPB:
+        #             toolbox.mate(child1, child2)
+        #             del child1.fitness.values
+        #             del child2.fitness.values
+        #
+        #     # Apply mutation on the offspring
+        #     for mutant in offspring:
+        #         if random.random() < MUTPB:
+        #             toolbox.mutate(mutant)
+        #             del mutant.fitness.values
+        #
+        #     # Evaluate the individuals with an invalid fitness
+        #     invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        #     fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        #     for ind, fit in list(zip(invalid_ind, fitnesses)):
+        #         ind.fitness.values = fit
+        #
+        #     # The population is entirely replaced by the offspring
+        #     pop[:] = offspring
+        #     record = stats.compile(pop)
+        #     popfit=[ind.fitness.values for ind in pop]
+        #     logbook.record(gen=g+1, evals=mu+(g+14)*lmbd,popfitnesses=popfit,pop=pop, **record)
+        #     with open('logbook.dat','wb') as f:
+        #         pickle.dump(logbook,f)
+        #     print(g)
+        #     logbook.header = "gen", "evals", "avg", "std", "min", "max"
+        #     print(logbook)
+        # print(self.sizes)
+        # print(pop)
+        # logbook.header = "gen", "evals", "avg", "std", "min", "max"
+        # print(logbook)
 
-            # Apply crossover on the offspring
-            #print(list(offspring))
-            #print(offspring[::2])
-            #print(offspring[1::2])
-            #print(zip(offspring[::2], offspring[1::2]))
-            for child1, child2 in list(zip(offspring[::2], offspring[1::2])):
-                if random.random() < CXPB:
-                    toolbox.mate(child1, child2)
-                    del child1.fitness.values
-                    del child2.fitness.values
+        gen = logbook.select("gen")
+        fit_avgs = logbook.select("avg")
+        fit_stds = logbook.select("std")
+        fit_mins = logbook.select("min")
+        fit_maxs = logbook.select("max")
 
-            # Apply mutation on the offspring
-            for mutant in offspring:
-                if random.random() < MUTPB:
-                    toolbox.mutate(mutant)
-                    del mutant.fitness.values
 
-            # Evaluate the individuals with an invalid fitness
-            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-            fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
-            for ind, fit in list(zip(invalid_ind, fitnesses)):
-                ind.fitness.values = fit
+        import matplotlib.pyplot as plt
 
-            # The population is entirely replaced by the offspring
-            pop[:] = offspring
-            print(g)
-        print(self.sizes)
-        print(pop)
+        fig, ax1 = plt.subplots()
+        ax1.plot(gen, fit_mins, "g-", label="Minimum Fitness")
+        ax1.plot(gen, fit_avgs, "y-", label="Average Fitness")
+        ax1.plot(gen, fit_stds, "k-", label="Std Fitness")
+        ax1.plot(gen, fit_maxs, "r-", label="Maximum Fitness")
+        ax1.set_xlabel("Generation")
+        ax1.set_ylabel("Fitness")
+        for tl in ax1.get_yticklabels():
+            tl.set_color("b")
+        plt.show()
+
+        #popfitnesses = logbook.select("popfitnesses")
+        #flatPopFit = []
+        #for popGenI in popfitnesses:
+        #    p = []
+        #    for indFit in popGenI:
+        #        p.append(indFit[0])
+        #    flatPopFit.append(p)
+
+
+
+        bestindvDict = {}
+        for i in range(len(self.modApp.varnames)):
+            bestindvDict[self.modApp.varnames[i]] = halloffame[0][i]
+            #bestindvDict[self.modApp.varnames[i]] = pop[np.argmin(flatPopFit[-1])][i]
+        #with open('retOptim.dat', 'wb') as f:
+        #    pickle.dump(pop[np.argmin(flatPopFit[-1])], f)
+
+        #bestindvDict=[]
+
+        with open('bestIndv.dat', 'wb') as f:
+            pickle.dump(bestindvDict, f)
+
+        return bestindvDict
+
